fac9cf3b5570c08c73c3695b4782c9cbffb47513
Léo TRUX <truxleo@gmail.com>
2024-04-29 11:36:44 +0100

    modal settings

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,7 +1,16 @@
 import Logo from '../../../public/images/AR_white.png'
 import History from './History/History.jsx'
+import {useState} from "react";
+import 'animate.css';
+
+
 const SideBar = () =>
 {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const handleSettingsClick = () => {
+        setIsModalOpen(!isModalOpen);
+    }
+
     return (
 
         <div className="sideBar">
@@ -9,14 +18,44 @@ const SideBar = () =>
                 <img src={Logo} alt="logo"/>
             </div>
             <History/>
-            <div className="settings">
+            <div className="settings" onClick={handleSettingsClick}>
                 <span className="user-icon">LT</span>
                 <p>Léo Trux</p>
                 <span  className="material-symbols-rounded">
                     expand_all
                 </span>
-
             </div>
+            {isModalOpen && (<div className="modal-settings animate__animated animate__fadeIn">
+                <div className="user-infos">
+                    <span>Léo Trux</span>
+                    <p>truxleo@gmail.com</p>
+                </div>
+                <div className="settings-line">
+                    <a>Chats deleted</a>
+                    <span className="material-symbols-rounded">
+                        delete
+                    </span>
+                </div>
+                <div className="settings-line">
+                    <a>Profile</a>
+                    <span className="material-symbols-rounded">
+                        account_circle
+                    </span>
+                </div>
+                <div className="settings-line">
+                    <a>Help</a>
+                    <span className="material-symbols-rounded">
+                        indeterminate_question_box
+                    </span>
+                </div>
+                <div className="settings-line">
+                    <a>Logout</a>
+                    <span className="material-symbols-rounded">
+                        logout
+                    </span>
+                </div>
+
+            </div>)}
         </div>
 
 
